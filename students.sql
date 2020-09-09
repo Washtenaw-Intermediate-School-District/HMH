2e5be0569acb3712ff7a6ba000006e3b54304057
Solomon Zheng <58829220+wisd-solomon@users.noreply.github.com>
2020-09-09 01:37:01 -0400

    duplicate fix
    fixed generating duplicate rows and integrated lookup for hmh org id

--- students.sql
+++ students.sql
@@ -16,7 +16,7 @@ WITH combined AS (
 		termid >= 3000
 )
 
-SELECT
+SELECT DISTINCT
 	'2020',
 	'S',
 	's-' || STUDENTS.dcid,
@@ -32,10 +32,21 @@ SELECT
 	STUDENTS.student_number || '@ycschools.us',
 	NULL,
 	'MDR',
-	NULL,
+	CASE 
+		WHEN combined.schoolid = 3000 THEN '88738122'
+		WHEN combined.schoolid = 1938 THEN '11963942'
+		WHEN combined.schoolid = 1153 THEN '516372'
+		WHEN combined.schoolid = 1157 THEN '516384'
+		WHEN combined.schoolid = 798 THEN '516279'
+		WHEN combined.schoolid = 1705 THEN '516293'
+		WHEN combined.schoolid = 2062 THEN '516401'
+		WHEN combined.schoolid = 9404 THEN '516267'
+		WHEN combined.schoolid = 1925 THEN '516475'
+		WHEN combined.schoolid = 2988 THEN '12168755'
+		WHEN combined.schoolid = 1923 THEN '516475'
+	END AS "OrgID",
 	STUDENTS.student_number || '@ycschools.us',
-	'ED',
-	combined.schoolid
+	'ED'
 	
 FROM STUDENTS
 	JOIN combined ON STUDENTS.id = combined.id
